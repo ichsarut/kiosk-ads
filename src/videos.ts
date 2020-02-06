bc37c2c689d2f1eb6db37d908432fe50b12cf088
ichsarut <ichsarut@gmail.com>
2020-02-07 04:01:28 +0700

    remove long videos

--- src/videos.ts
+++ src/videos.ts
@@ -1,23 +1,23 @@
 const videos = [
-  {
-    description:
-      "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-    ],
-    subtitle: "By Blender Foundation",
-    thumb: "images/BigBuckBunny.jpg",
-    title: "Big Buck Bunny"
-  },
-  {
-    description: "The first Blender Open Movie from 2006",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
-    ],
-    subtitle: "By Blender Foundation",
-    thumb: "images/ElephantsDream.jpg",
-    title: "Elephant Dream"
-  },
+  // {
+  //   description:
+  //     "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+  //   ],
+  //   subtitle: "By Blender Foundation",
+  //   thumb: "images/BigBuckBunny.jpg",
+  //   title: "Big Buck Bunny"
+  // },
+  // {
+  //   description: "The first Blender Open Movie from 2006",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+  //   ],
+  //   subtitle: "By Blender Foundation",
+  //   thumb: "images/ElephantsDream.jpg",
+  //   title: "Elephant Dream"
+  // },
   {
     description:
       "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
@@ -67,66 +67,66 @@ const videos = [
     subtitle: "By Google",
     thumb: "images/ForBiggerMeltdowns.jpg",
     title: "For Bigger Meltdowns"
-  },
-  {
-    description:
-      "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
-    ],
-    subtitle: "By Blender Foundation",
-    thumb: "images/Sintel.jpg",
-    title: "Sintel"
-  },
-  {
-    description:
-      "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
-    ],
-    subtitle: "By Garage419",
-    thumb: "images/SubaruOutbackOnStreetAndDirt.jpg",
-    title: "Subaru Outback On Street And Dirt"
-  },
-  {
-    description:
-      "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
-    ],
-    subtitle: "By Blender Foundation",
-    thumb: "images/TearsOfSteel.jpg",
-    title: "Tears of Steel"
-  },
-  {
-    description:
-      "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4"
-    ],
-    subtitle: "By Garage419",
-    thumb: "images/VolkswagenGTIReview.jpg",
-    title: "Volkswagen GTI Review"
-  },
-  {
-    description:
-      "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
-    ],
-    subtitle: "By Garage419",
-    thumb: "images/WeAreGoingOnBullrun.jpg",
-    title: "We Are Going On Bullrun"
-  },
-  {
-    description:
-      "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
-    sources: [
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
-    ],
-    subtitle: "By Garage419",
-    thumb: "images/WhatCarCanYouGetForAGrand.jpg",
-    title: "What care can you get for a grand?"
   }
+  // {
+  //   description:
+  //     "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
+  //   ],
+  //   subtitle: "By Blender Foundation",
+  //   thumb: "images/Sintel.jpg",
+  //   title: "Sintel"
+  // },
+  // {
+  //   description:
+  //     "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
+  //   ],
+  //   subtitle: "By Garage419",
+  //   thumb: "images/SubaruOutbackOnStreetAndDirt.jpg",
+  //   title: "Subaru Outback On Street And Dirt"
+  // },
+  // {
+  //   description:
+  //     "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+  //   ],
+  //   subtitle: "By Blender Foundation",
+  //   thumb: "images/TearsOfSteel.jpg",
+  //   title: "Tears of Steel"
+  // },
+  // {
+  //   description:
+  //     "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4"
+  //   ],
+  //   subtitle: "By Garage419",
+  //   thumb: "images/VolkswagenGTIReview.jpg",
+  //   title: "Volkswagen GTI Review"
+  // },
+  // {
+  //   description:
+  //     "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
+  //   ],
+  //   subtitle: "By Garage419",
+  //   thumb: "images/WeAreGoingOnBullrun.jpg",
+  //   title: "We Are Going On Bullrun"
+  // },
+  // {
+  //   description:
+  //     "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
+  //   sources: [
+  //     "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
+  //   ],
+  //   subtitle: "By Garage419",
+  //   thumb: "images/WhatCarCanYouGetForAGrand.jpg",
+  //   title: "What care can you get for a grand?"
+  // }
 ]
 export default videos
